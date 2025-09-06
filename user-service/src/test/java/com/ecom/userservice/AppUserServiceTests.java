3afd309dc5adc68579e117d62a8663d10134b404
pankaj <kmrpnk@gmail.com>
2025-09-06 15:03:54 +0530

    user-service: added unit test for AppUserService

--- user-service/src/test/java/com/ecom/userservice/AppUserServiceTests.java
+++ user-service/src/test/java/com/ecom/userservice/AppUserServiceTests.java
@@ -1,7 +1,9 @@
 package com.ecom.userservice;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -23,7 +26,7 @@ class AppUserServiceTests {
 
     @Mock
     private AppUserRepository appUserRepository;
-    @Mock
+    @Spy
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Mock
     private IDGenerator idGenerator;
@@ -32,21 +35,62 @@ class AppUserServiceTests {
 
     AppUser appUser;
 
+    private final String phone="+919454243912";
+    private final String name="pankaj";
+    private final String password="password";
+    private final String encodedPass="$2a$14$D0ESj9dZ/jlzMRBkFSg9guUrhKuY14s7vonVkF0rBZEletStXYFEu";
+    private final String email="email@gamil.com";
+
+
     @BeforeEach
     void setUp(){
-        appUser=new AppUser(null, "+919454243912", "pankaj", "password", "email@gamil.com");
+        bCryptPasswordEncoder=new BCryptPasswordEncoder(14);
+        appUser=new AppUser(null, phone, name, encodedPass, email);
     }
 
     @Test
     void testCreateAppUser(){
         when(appUserRepository.save(any(AppUser.class))).thenReturn(appUser);
         when(idGenerator.next()).thenReturn(11l);
-        when(bCryptPasswordEncoder.encode(any(CharSequence.class))).thenReturn("xxx");
-
         AppUser createdAppUser=appUserService.createAppUser(appUser);
         assertNotNull(createdAppUser);
-
         assertEquals(11l, createdAppUser.getId());
     }
 
+    @Test
+    void testGetAppUserByPhoneNumber(){
+        when(appUserRepository.getByPhoneNumber(phone)).thenReturn(appUser);
+        AppUser foundAppUser=appUserService.getAppUserByPhoneNumber(phone);
+        assertNotNull(foundAppUser);
+        assertEquals(appUser, foundAppUser);
+        assertEquals(appUser.getName(), foundAppUser.getName());
+    }
+
+    @Test
+    void testGetAppUserByUsername(){
+        when(appUserRepository.getByName(name)).thenReturn(appUser);
+        AppUser foundAppUser=appUserService.getAppUserByUsername(name);
+        assertNotNull(foundAppUser);
+        assertEquals(appUser, foundAppUser);
+        assertEquals(appUser.getPhoneNumber(), foundAppUser.getPhoneNumber());
+    }
+
+    @Test
+    void testVerifyUsernamePassword(){
+        when(appUserRepository.getByName(name)).thenReturn(appUser);
+        Boolean verified=appUserService.verifyUsernamePassword(name, password);
+        assertTrue(verified);
+
+        verified=appUserService.verifyUsernamePassword(name, password+"wrong");
+        assertFalse(verified);
+    }
+
+    @Test
+    void testGetAppUserByEmail(){
+        when(appUserRepository.getByEmail(email)).thenReturn(appUser);
+        AppUser foundAppUser=appUserService.getAppUserByEmail(email);
+        assertNotNull(foundAppUser);
+        assertEquals(appUser, foundAppUser);
+        assertEquals(appUser.getPhoneNumber(), foundAppUser.getPhoneNumber());
+    }
 }
